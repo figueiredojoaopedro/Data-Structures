bcb8eb8793d71b4eec577b7807de5714ef021722
figueiredojoaopedro <neewprojecti@gmail.com>
2022-07-03 15:52:08 -0300

    big o o(n) learned

--- bigO/O(n).js
+++ bigO/O(n).js
@@ -6,13 +6,16 @@ const largeArray = new Array(100000).fill('nemo');
 
 // we can make a function to find him
 function findNemo (array){
-    let t0 = performance.now();
     for(let i = 0; i < array.length; i++){
         if(array[i] === 'nemo')
             console.log("Found nemo mate!!!");
     }
-    let t1 = performance.now();
-    console.log('it took: ' + (t1 - t0) + "milliseconds!");
 }
 
 findNemo(largeArray);
+
+// O(n) is the very fist big o notation
+// it states that no matter the number of operations it does, the number of elements calculated will be exactly linear with the number of the operations
+// like a linear chart, with only one variable and no coeficients to add.
+// O(n) = n -> the equation of the chart
+// the other name can be Linear Time.
